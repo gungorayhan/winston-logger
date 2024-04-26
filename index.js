59d48ed9e02b51aa149e36c27583972104c003dc
AYHAN GÜNGÖR <gngr.ayhan@gmail.com>
2024-04-26 14:31:57 +0300

    printf

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const winston = require("winston")
+const {combine, timestamp, printf} = winston.format
 
 const logger = winston.createLogger({
     level:"info",
-    format:winston.format.json(),
+    format:combine(
+        timestamp(),
+        printf((info)=>`${info.timestamp} ${info.level} ${info.message}`)
+    ),
     transports:[
         new winston.transports.Console(),
-        new winston.transports.File({
-            filename:"app.log",
-            level:"info"
-        })
+        // new winston.transports.File({
+        //     filename:"app.log",
+        //     level:"info"
+        // })
     ]
 })
 
 
 logger.info("An info log")
-logger.error("An error log")
