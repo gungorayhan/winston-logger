ecea03535fae591fce159ae7e230def936cfa307
AYHAN GÜNGÖR <gngr.ayhan@gmail.com>
2024-04-26 14:37:06 +0300

    logger.child

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const winston = require("winston")
-const {combine, timestamp, printf} = winston.format
+const {combine, timestamp, printf,json,prettyPrint} = winston.format
 
 const logger = winston.createLogger({
     level:"info",
     format:combine(
         timestamp(),
-        printf((info)=>`${info.timestamp} ${info.level} ${info.message}`)
+        // printf((info)=>`${info.timestamp} ${info.level} ${info.message}`)
+        json()
     ),
     transports:[
         new winston.transports.Console(),
-        // new winston.transports.File({
-        //     filename:"app.log",
-        //     level:"info"
-        // })
+        new winston.transports.File({
+            filename:"app.log",
+            level:"info"
+        })
     ]
 })
 
+const requestLog = {method:"Get",isAuthenticated:false}
+const childLogger = logger.child(requestLog)
 
-logger.info("An info log")
+logger.info("An info log",requestLog)
+childLogger.info("An info log. childlogger")
